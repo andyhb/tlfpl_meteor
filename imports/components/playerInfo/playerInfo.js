a8e807ec2be25b5e1f117f72e5911b8aaad0a7ca
andyhb <30803001+andyhb@users.noreply.github.com>
2018-08-30 00:33:20 +0100

    Started player info page

---
+++ imports/components/playerInfo/playerInfo.js
@@ -0,0 +1,66 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import {Players} from '../../api/players.js';
+import './playerInfo.html';
+
+Template.playerInfo.onCreated(function bodyOnCreated() {
+    Meteor.subscribe('playerInfo', FlowRouter.getParam('playerId') + "/" + FlowRouter.getParam('seasonId'));
+});
+
+Template.playerInfo.helpers({
+    playerInfo() {
+        return Players.findOne();
+    },
+    showWebName() {
+        return this.Surname !== this.WebName;
+    },
+    getInfoIconType() {
+        return getInfoIconType(this.ChanceOfPlayingPercentage);
+    },
+    getNews() {
+        if (this.News === "") {
+            return "Player available";
+        } else {
+            return this.News;
+        }
+    },
+    getAlertClass() {
+        let iconType = getInfoIconType(this.ChanceOfPlayingPercentage);
+
+        if (iconType === "check") {
+            return "success";
+        }
+
+        if (iconType == "exclamation") {
+            return "warning";
+        }
+
+        return "danger";
+    },
+    getFixture(fixture) {
+        return (fixture.Home ?
+             "<b>" + this.TeamName + "</b> v " + fixture.Opponent : 
+             fixture.Opponent + " v <b>" + this.TeamName + "</b>");
+    },
+    getGameweek(next) {
+        let g = Globals.findOne();
+
+        if (g) {
+            return (next ? g.Gameweek + 1 : g.Gameweek);
+        }
+    }
+});
+
+const getInfoIconType = function(chance) {
+    if (chance < 0 || chance === 100) {
+        return "check";
+    }
+
+    if (chance === 0) {
+        return "exclamation-triangle";
+    }
+    
+    return "exclamation";
+};
