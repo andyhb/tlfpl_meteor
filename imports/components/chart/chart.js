4849723d99e810a634c2e247afd37338194b2d4e
andyhb <30803001+andyhb@users.noreply.github.com>
2018-09-24 22:56:05 +0100

    Custom colour / pink chart toggle

--- imports/components/chart/chart.js
+++ imports/components/chart/chart.js
@@ -6,6 +6,8 @@ import Chart from 'chart.js';
 import {Table} from '../../api/table.js';
 import './chart.html';
 
+const pinkMode = new ReactiveVar(true);
+
 Template.chart.onCreated(function bodyOnCreated() {
     Meteor.subscribe("table");
 });
@@ -26,31 +28,35 @@ Template.chart.onRendered(function () {
                 } else {
                     teamData = {};
                     teamData.label = teamStanding.TeamName;
-                    if (teamStanding.TeamName === "Rich's Belly Floppers") {
-                        teamData.borderColor = 'rgb(151,245,245)';
-                    } else if (teamStanding.TeamName === "Cotton Pickers") {
-                        teamData.borderColor = 'rgb(176,128,64)';
-                    } else if (teamStanding.TeamName === "TBA FC") {
-                        teamData.borderColor = 'rgb(227,37,107)';
-                    } else if (teamStanding.TeamName === "Dyslexia Untied") {
-                        teamData.borderColor = 'rgb(40,106,77)';
-                    } else if (teamStanding.TeamName === "FC Quichetopol") {
-                        teamData.borderColor = 'rgb(236,213,64)';
-                    } else if (teamStanding.TeamName === "Bangkok Young Boys") {
-                        teamData.borderColor = 'rgb(183,132,167)';
-                    } else if (teamStanding.TeamName === "Lads On Toure") {
-                        teamData.borderColor = 'rgb(255,0,0)';
-                        teamData.borderDash = [5,5];
-                    } else if (teamStanding.TeamName === "Pinky's U-11s") {
-                        teamData.borderColor = 'rgb(50,50,255)';
-                    } else if (teamStanding.TeamName === "BJ Falls FC") {
-                        teamData.borderColor = 'rgb(110,238,110)';
-                    } else {
+                    if (pinkMode.get()) {
                         teamData.borderColor = 'rgb(255,99,132)';
+                    } else {
+                        if (teamStanding.TeamName === "Rich's Belly Floppers") {
+                            teamData.borderColor = 'rgb(151,245,245)';
+                        } else if (teamStanding.TeamName === "Cotton Pickers") {
+                            teamData.borderColor = 'rgb(176,128,64)';
+                        } else if (teamStanding.TeamName === "TBA FC") {
+                            teamData.borderColor = 'rgb(227,37,107)';
+                        } else if (teamStanding.TeamName === "Dyslexia Untied") {
+                            teamData.borderColor = 'rgb(40,106,77)';
+                        } else if (teamStanding.TeamName === "FC Quichetopol") {
+                            teamData.borderColor = 'rgb(236,213,64)';
+                        } else if (teamStanding.TeamName === "Bangkok Young Boys") {
+                            teamData.borderColor = 'rgb(183,132,167)';
+                        } else if (teamStanding.TeamName === "Lads On Toure") {
+                            teamData.borderColor = 'rgb(255,0,0)';
+                            teamData.borderDash = [5,5];
+                        } else if (teamStanding.TeamName === "Pinky's U-11s") {
+                            teamData.borderColor = 'rgb(50,50,255)';
+                        } else if (teamStanding.TeamName === "BJ Falls FC") {
+                            teamData.borderColor = 'rgb(110,238,110)';
+                        } else {
+                            teamData.borderColor = 'rgb(255,99,132)';
+                        }
+                        teamData.backgroundColor = teamData.borderColor;
+                        teamData.fill = false;
                     }
-                    teamData.backgroundColor = teamData.borderColor;
                     teamData.data = [teamStanding.Position];
-                    teamData.fill = false;
                     tableDatasets.push(teamData);
                 }
             });
@@ -74,4 +80,10 @@ Template.chart.onRendered(function () {
             }
         });
     });
+});
+
+Template.chart.events({
+    'click #customColours'() {
+        pinkMode.set(!pinkMode.get());
+    }
 });
