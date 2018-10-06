01891b1290f8591d0c4d1f6a2887a8fbaf55d970
andyhb <30803001+andyhb@users.noreply.github.com>
2018-10-07 00:02:19 +0100

    Cup tables

--- imports/components/cup/fixtures.js
+++ imports/components/cup/fixtures.js
@@ -2,11 +2,14 @@ import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
-import {CupGroupFixtures} from '../../api/cup.js';
+import {CupGroupFixtures, CupGroupTables} from '../../api/cup.js';
 import './fixtures.html';
+import './cupGroupTable.html';
+import './cupGroupTableEntry.html';
 
 Template.fixtures.onCreated(function bodyOnCreated() {
     Meteor.subscribe("cup_group_fixtures");
+    Meteor.subscribe("cup_group_tables");
 });
 
 Template.fixtures.helpers({
@@ -15,5 +18,100 @@ Template.fixtures.helpers({
     },
     notBye(match) {
         return !match.Team1.Bye && !match.Team2.Bye;
+    },
+    getCupTableData(id) {
+        var g = Globals.findOne();
+
+        if (g) {
+            var gameweek = gameweekState.get();
+            globalGameweek = g.Gameweek;
+
+            return CupGroupTables.findOne({
+                _id: (gameweek ? gameweek : globalGameweek) + "/" + g.SeasonId + "/" + id
+            });
+        }
     }
+});
+
+const gameweekState = new ReactiveVar();
+let globalGameweek = 1;
+
+Template.cupGroupTable.onCreated(function bodyOnCreated() {
+    Meteor.subscribe("cup_group_tables");
+});
+
+Template.cupGroupTable.helpers({
+    getGameweek() {
+        if (this.data) {
+            return this.data.Gameweek;
+        }
+    },
+    showNextGameweekButton() {
+        if (this.data) {
+            var gameweek = this.data.Gameweek + this.skipWeek;
+
+            if (gameweek > globalGameweek) {
+                return false;
+            }
+
+            return true;
+        }
+    },
+    showPreviousGameweekButton() {
+        if (this.data) {
+            var gameweek = this.data.Gameweek - this.skipWeek;
+
+            if (gameweek < this.startingWeek) {
+                return false;
+            }
+
+            return true;
+        }
+    }
+});
+
+Template.cupGroupTable.events({
+    'click [nextGameweek]'() {
+      var gameweek = this.data.Gameweek + this.skipWeek;
+  
+      if (gameweek > globalGameweek) {
+        gameweek = globalGameweek;
+      }
+  
+      gameweekState.set(gameweek);
+    },
+    'click [previousGameweek]'() {
+      var gameweek = this.data.Gameweek - this.skipWeek;
+  
+      if (gameweek < this.startingWeek) {
+        gameweek = this.startingWeek;
+      }
+  
+      gameweekState.set(gameweek);
+    }
+});
+
+Template.cupGroupTableEntry.helpers({
+    movedUp(position, previousPosition) {
+        if (!previousPosition) {
+          return false;
+        }
+    
+        if (position < previousPosition) {
+          return true;
+        }
+    
+        return false;
+      },
+      movedDown(position, previousPosition) {
+        if (!previousPosition) {
+          return false;
+        }
+    
+        if (position > previousPosition) {
+          return true;
+        }
+    
+        return false;
+      }
 });
