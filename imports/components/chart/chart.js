80558c58613062ac7d0c89a8abbd3e27896ac32b
Andy HB <andyhb@pebblepad.co.uk>
2019-01-22 13:14:34 +0000

    Cater for Wills team on graph properly

--- imports/components/chart/chart.js
+++ imports/components/chart/chart.js
@@ -66,7 +66,7 @@ Template.chart.onRendered(function () {
                             teamData.borderColor = 'rgb(176,128,64)';
                         } else if (teamStanding.TeamName === "TBA FC") {
                             teamData.borderColor = 'rgb(227,37,107)';
-                        } else if (teamStanding.TeamName === "Dyslexia Untied" || "Victor Moses Lawn") {
+                        } else if (teamStanding.TeamName === "Dyslexia Untied" || teamStanding.TeamName ===  "Victor Moses Lawn") {
                             teamData.borderColor = 'rgb(40,106,77)';
                         } else if (teamStanding.TeamName === "FC Quichetopol") {
                             teamData.borderColor = 'rgb(236,213,64)';
