1c297554c7954641386aa85f17866fb4442ce31d
Andy HB <andyhb@pebblepad.co.uk>
2019-09-03 12:48:38 +0100

    Updated hall of fame to show standings per season

--- imports/components/hallOfFame/hallOfFame.js
+++ imports/components/hallOfFame/hallOfFame.js
@@ -1,64 +1,96 @@
-import { Meteor } from 'meteor/meteor';
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import { Meteor } from "meteor/meteor";
+import { Template } from "meteor/templating";
+import { ReactiveVar } from "meteor/reactive-var";
 
-import {HallOfFame} from '../../api/hallOfFame.js';
-import './hallOfFame.html';
+import { HallOfFame } from "../../api/hallOfFame.js";
+import "./hallOfFame.html";
 
-Template.hallOfFame.onCreated(function bodyOnCreated () {
-    Meteor.subscribe("halloffame");
+Template.hallOfFame.onCreated(function bodyOnCreated() {
+  Meteor.subscribe("halloffame");
 });
 
 Template.hallOfFame.helpers({
-    getLeagueResults() {
-        let hallOfFame = HallOfFame.findOne();
+  getLeagueResults() {
+    let hallOfFame = HallOfFame.findOne();
 
-        if (hallOfFame) {
-            return hallOfFame.LeagueResults.sort(sortLeagueResults);
-        }
-    },
-    getPosition(index) {
-        return index + 1;
-    },
-    getCupResults() {
-        let hallOfFame = HallOfFame.findOne();
-
-        if (hallOfFame) {
-            return hallOfFame.CupResults.sort(sortCupResults);
-        }
-    },
-    getTrophies(count)  {
-        return Array.from(Array(count));
+    if (hallOfFame) {
+      return hallOfFame.LeagueResults.sort(sortLeagueResults);
     }
+  },
+  getPosition(index) {
+    return index + 1;
+  },
+  getCupResults() {
+    let hallOfFame = HallOfFame.findOne();
+
+    if (hallOfFame) {
+      return hallOfFame.CupResults.sort(sortCupResults);
+    }
+  },
+  getTrophies(count) {
+    return Array.from(Array(count));
+  },
+  getSeasons() {
+    let hallOfFame = HallOfFame.findOne();
+
+    const seasons = [];
+    if (hallOfFame) {
+      hallOfFame.LeagueResults[0].SeasonStandings.forEach(ss => {
+        seasons.push(ss.SeasonName);
+      });
+    }
+    return seasons;
+  },
+  getLeaguePosition(season, leagueResults) {
+    if (leagueResults) {
+      const slr = leagueResults.filter(lr => {
+        return lr.SeasonName === season;
+      })[0];
+
+      if (slr) {
+        if (slr.Position === 1) {
+          return `<span class="fa fa-trophy winner"></span>`;
+        }
+
+        if (slr.Position === 2) {
+          return `<span class="fa fa-trophy runnerUp"></span>`;
+        }
+
+        return slr.Position;
+      }
+    }
+
+    return "-";
+  }
 });
 
 const sortLeagueResults = function(a, b) {
-    let aTotalPoints = a.TotalPoints;
-    let bTotalPoints = b.TotalPoints;
+  let aTotalPoints = a.TotalPoints;
+  let bTotalPoints = b.TotalPoints;
 
-    let aHOFPoints = a.HallOfFamePoints;
-    let bHOFPoints = b.HallOfFamePoints;
+  let aHOFPoints = a.HallOfFamePoints;
+  let bHOFPoints = b.HallOfFamePoints;
 
-    if (aHOFPoints > bHOFPoints) return -1;
-    if (aHOFPoints < bHOFPoints) return 1;
-    if (aTotalPoints < bTotalPoints) return 1;
-    if (aTotalPoints > bTotalPoints) return -1;
+  if (aHOFPoints > bHOFPoints) return -1;
+  if (aHOFPoints < bHOFPoints) return 1;
+  if (aTotalPoints < bTotalPoints) return 1;
+  if (aTotalPoints > bTotalPoints) return -1;
 };
 
 const sortCupResults = function(a, b) {
-    let aWins = a.CupWinner;
-    let bWins = b.CupWinner;
+  let aWins = a.CupWinner;
+  let bWins = b.CupWinner;
 
-    let aRunnerUp = a.CupRunnerUp;
-    let bRunnerUp = b.CupRunnerUp;
+  let aRunnerUp = a.CupRunnerUp;
+  let bRunnerUp = b.CupRunnerUp;
 
-    let aName = a.ManagerName;
-    let bName = b.ManagerName;
+  let aName = a.ManagerName;
+  let bName = b.ManagerName;
 
-    if (aWins > bWins) return -1;
-    if (aWins < bWins) return 1;
-    if (aRunnerUp > bRunnerUp) return -1;
-    if (aRunnerUp < bRunnerUp) return 1;
-    if (aName > bName) return 1;
-    if (aName < bName) return -1;
+  if (aWins > bWins) return -1;
+  if (aWins < bWins) return 1;
+  if (aRunnerUp > bRunnerUp) return -1;
+  if (aRunnerUp < bRunnerUp) return 1;
+  if (aName > bName) return 1;
+  if (aName < bName) return -1;
 };
