5cfe7dcc1ed46be804ebdff6f7d2fb71794786ce
andyhb <30803001+andyhb@users.noreply.github.com>
2019-09-28 16:08:39 +0100

    Added season check to cup fixtures on home page

--- imports/components/cup/cupFixtures.js
+++ imports/components/cup/cupFixtures.js
@@ -1,83 +1,91 @@
-import { Meteor } from 'meteor/meteor';
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import { Meteor } from "meteor/meteor";
+import { Template } from "meteor/templating";
+import { ReactiveVar } from "meteor/reactive-var";
 
-import {CupGroupFixtures} from '../../api/cup.js';
-import './cupFixtures.html';
+import { CupGroupFixtures } from "../../api/cup.js";
+import "./cupFixtures.html";
 
 const cupGameweek = new ReactiveVar();
 
 Template.cupFixtures.onCreated(function bodyOnCreated() {
-    Meteor.subscribe("cup_group_fixtures");
+  Meteor.subscribe("cup_group_fixtures");
 });
 
 Template.home.onCreated(function bodyOnCreated() {
-    Meteor.subscribe("cup_group_fixtures");
+  Meteor.subscribe("cup_group_fixtures");
 });
 
 Template.home.helpers({
-    cupWeek() {
-        let g = Globals.findOne();
+  cupWeek() {
+    let g = Globals.findOne();
 
-        if (g) {
-            return CupGroupFixtures.find({
-                $or: [{"Fixtures.Gameweek": g.Gameweek},{"Fixtures.Gameweek": g.Gameweek + 1}]
-            }).count();
-        }
-
-        return false;
+    if (g) {
+      return CupGroupFixtures.find({
+        SeasonId: g.SeasonId,
+        $or: [
+          { "Fixtures.Gameweek": g.Gameweek },
+          { "Fixtures.Gameweek": g.Gameweek + 1 }
+        ]
+      }).count();
     }
+
+    return false;
+  }
 });
 
 Template.cupFixtures.helpers({
-    fixturesForGameweek() {
-        let g = Globals.findOne();
+  fixturesForGameweek() {
+    let g = Globals.findOne();
 
-        if (g) {
-            let cupGroupFixtures = CupGroupFixtures.find({
-                $or: [{"Fixtures.Gameweek": g.Gameweek},{"Fixtures.Gameweek": g.Gameweek + 1}]
+    if (g) {
+      let cupGroupFixtures = CupGroupFixtures.find({
+        SeasonId: g.SeasonId,
+        $or: [
+          { "Fixtures.Gameweek": g.Gameweek },
+          { "Fixtures.Gameweek": g.Gameweek + 1 }
+        ]
+      });
+
+      let fixtures = [];
+      let fixtureGameweeks = [];
+
+      cupGroupFixtures.forEach(function(group) {
+        group.Fixtures.forEach(function(week) {
+          if (week.Gameweek == g.Gameweek || week.Gameweek == g.Gameweek + 1) {
+            week.Matches.forEach(function(match) {
+              if (!match.Team1.Bye && !match.Team2.Bye) {
+                fixtureGameweeks.push(week.Gameweek);
+                match.GroupName = group.GroupName;
+                fixtures.push(match);
+              }
             });
+          }
+        });
+      });
 
-            let fixtures = [];
-            let fixtureGameweeks = [];
-
-            cupGroupFixtures.forEach(function(group) {
-                group.Fixtures.forEach(function(week) {
-                if (week.Gameweek == g.Gameweek || week.Gameweek == g.Gameweek + 1) {
-                    week.Matches.forEach(function(match) {
-                        if (!match.Team1.Bye && !match.Team2.Bye) {
-                            fixtureGameweeks.push(week.Gameweek);
-                            match.GroupName = group.GroupName;
-                            fixtures.push(match);
-                        }
-                    });
-                }
-                });
-            });
-
-            if (fixtures.length > 0) {
-                let uniqueGameweeks = fixtureGameweeks.filter(onlyUnique);
-                cupGameweek.set(uniqueGameweeks.join('/'));
-                return fixtures;
-            }
-        }
-    },
-    getGameweek() {
-        return cupGameweek.get();
-    },
-    isGreater(fixture, team1) {
-        if (team1) {
-            if (fixture.Team1.Points > fixture.Team2.Points) {
-                return "isGreater";
-            }
-        } else {
-            if (fixture.Team2.Points > fixture.Team1.Points) {
-                return "isGreater";
-            }
-        }
+      if (fixtures.length > 0) {
+        let uniqueGameweeks = fixtureGameweeks.filter(onlyUnique);
+        cupGameweek.set(uniqueGameweeks.join("/"));
+        return fixtures;
+      }
     }
+  },
+  getGameweek() {
+    return cupGameweek.get();
+  },
+  isGreater(fixture, team1) {
+    if (team1) {
+      if (fixture.Team1.Points > fixture.Team2.Points) {
+        return "isGreater";
+      }
+    } else {
+      if (fixture.Team2.Points > fixture.Team1.Points) {
+        return "isGreater";
+      }
+    }
+  }
 });
 
-const onlyUnique = function(value, index, self) { 
-    return self.indexOf(value) === index;
-}
+const onlyUnique = function(value, index, self) {
+  return self.indexOf(value) === index;
+};
