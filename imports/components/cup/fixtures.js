ff455d548918fb078bce1fd78dc97a4db56a4d06
andyhb <30803001+andyhb@users.noreply.github.com>
2020-12-27 17:34:24 +0000

    Reworked cup table next and previous

--- imports/components/cup/fixtures.js
+++ imports/components/cup/fixtures.js
@@ -5,7 +5,7 @@ import { ReactiveVar } from "meteor/reactive-var";
 import {
   CupGroupFixtures,
   CupGroupTables,
-  CupKnockoutFixtures
+  CupKnockoutFixtures,
 } from "../../api/cup.js";
 import "./fixtures.html";
 import "./cupGroupTable.html";
@@ -33,8 +33,8 @@ Template.fixtures.helpers({
         { SeasonId: g.SeasonId },
         {
           sort: {
-            RoundOrder: -1
-          }
+            RoundOrder: -1,
+          },
         }
       );
     }
@@ -57,7 +57,7 @@ Template.fixtures.helpers({
 
       if (tables) {
         var gw = gameweek ? gameweek : globalGameweek;
-        var selectedTable = tables.filter(table => table.Gameweek === gw)[0];
+        var selectedTable = tables.filter((table) => table.Gameweek === gw)[0];
 
         if (!selectedTable) {
           var orderedTables = tables.sort(sortTables);
@@ -88,10 +88,10 @@ Template.fixtures.helpers({
     } else {
       return "";
     }
-  }
+  },
 });
 
-const sortTables = function(a, b) {
+const sortTables = function (a, b) {
   let aGameweek = a.Gameweek;
   let bGameweek = b.Gameweek;
 
@@ -101,9 +101,11 @@ const sortTables = function(a, b) {
 
 const gameweekState = new ReactiveVar();
 let globalGameweek = 1;
+let fixtureWeeks = [];
 
 Template.cupGroupTable.onCreated(function bodyOnCreated() {
   Meteor.subscribe("cup_group_tables");
+  Meteor.subscribe("cup_group_fixtures");
 });
 
 Template.cupGroupTable.helpers({
@@ -113,8 +115,27 @@ Template.cupGroupTable.helpers({
     }
   },
   showNextGameweekButton() {
-    if (this.data) {
-      var gameweek = this.data.Gameweek + this.skipWeek;
+    var g = Globals.findOne();
+
+    if (this.data && g) {
+      let tempCGF = CupGroupFixtures.find({
+        _id: this.data.CupGroupId,
+      });
+
+      if (tempCGF) {
+        tempCGF.forEach((groupFixtures) => {
+          groupFixtures.Fixtures.forEach((fixture) => {
+            fixtureWeeks.push(fixture.Gameweek);
+          });
+        });
+      }
+
+      var gameweek = this.data.Gameweek + 1;
+
+      currIndex = fixtureWeeks.indexOf(this.data.Gameweek);
+      if (currIndex >= 0 && currIndex < fixtureWeeks.length - 1) {
+        gameweek = fixtureWeeks[currIndex + 1];
+      }
 
       if (gameweek > globalGameweek || gameweek > 26) {
         return false;
@@ -124,37 +145,98 @@ Template.cupGroupTable.helpers({
     }
   },
   showPreviousGameweekButton() {
-    if (this.data) {
-      var gameweek = this.data.Gameweek - this.skipWeek;
+    var g = Globals.findOne();
 
-      if (gameweek < this.startingWeek) {
+    if (this.data && g) {
+      let tempCGF = CupGroupFixtures.find({
+        _id: this.data.CupGroupId,
+      });
+
+      if (tempCGF) {
+        tempCGF.forEach((groupFixtures) => {
+          groupFixtures.Fixtures.forEach((fixture) => {
+            fixtureWeeks.push(fixture.Gameweek);
+          });
+        });
+      }
+
+      var gameweek = this.data.Gameweek - 1;
+
+      currIndex = fixtureWeeks.indexOf(this.data.Gameweek);
+      if (currIndex >= 0 && currIndex < fixtureWeeks.length - 1) {
+        gameweek = fixtureWeeks[currIndex - 1];
+      }
+
+      if (gameweek === undefined || gameweek < this.startingWeek) {
         return false;
       }
 
       return true;
     }
-  }
+  },
 });
 
 Template.cupGroupTable.events({
   "click [nextGameweek]"() {
-    var gameweek = this.data.Gameweek + this.skipWeek;
+    var g = Globals.findOne();
 
-    if (gameweek > globalGameweek) {
-      gameweek = globalGameweek;
+    if (this.data && g) {
+      let tempCGF = CupGroupFixtures.find({
+        _id: this.data.CupGroupId,
+      });
+
+      if (tempCGF) {
+        tempCGF.forEach((groupFixtures) => {
+          groupFixtures.Fixtures.forEach((fixture) => {
+            fixtureWeeks.push(fixture.Gameweek);
+          });
+        });
+      }
+
+      var gameweek = this.data.Gameweek + 1;
+
+      currIndex = fixtureWeeks.indexOf(this.data.Gameweek);
+      if (currIndex >= 0 && currIndex < fixtureWeeks.length - 1) {
+        gameweek = fixtureWeeks[currIndex + 1];
+      }
+
+      if (gameweek > globalGameweek) {
+        gameweek = globalGameweek;
+      }
+
+      gameweekState.set(gameweek);
     }
-
-    gameweekState.set(gameweek);
   },
   "click [previousGameweek]"() {
-    var gameweek = this.data.Gameweek - this.skipWeek;
+    var g = Globals.findOne();
 
-    if (gameweek < this.startingWeek) {
-      gameweek = this.startingWeek;
+    if (this.data && g) {
+      let tempCGF = CupGroupFixtures.find({
+        _id: this.data.CupGroupId,
+      });
+
+      if (tempCGF) {
+        tempCGF.forEach((groupFixtures) => {
+          groupFixtures.Fixtures.forEach((fixture) => {
+            fixtureWeeks.push(fixture.Gameweek);
+          });
+        });
+      }
+
+      var gameweek = this.data.Gameweek - 1;
+
+      currIndex = fixtureWeeks.indexOf(this.data.Gameweek);
+      if (currIndex >= 0 && currIndex < fixtureWeeks.length - 1) {
+        gameweek = fixtureWeeks[currIndex - 1];
+      }
+
+      if (gameweek < this.startingWeek) {
+        gameweek = this.startingWeek;
+      }
+
+      gameweekState.set(gameweek);
     }
-
-    gameweekState.set(gameweek);
-  }
+  },
 });
 
 Template.cupGroupTableEntry.helpers({
@@ -182,5 +264,5 @@ Template.cupGroupTableEntry.helpers({
   },
   getPlayed() {
     return this.Win + this.Lose + this.Draw;
-  }
+  },
 });
